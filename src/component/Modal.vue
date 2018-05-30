f370385e83bfaa4432bed000f09fa87ffb5f8577
ThadeuJose <thadeujs@gmail.com>
2018-05-30 16:42:50 -0700

    Create of modal.vue

---
+++ src/component/Modal.vue
@@ -0,0 +1,18 @@
+<template>	
+  <div v-if='visible'>Modal{{visible}}{{item}}</div>	
+</template>
+
+<script>
+  export default { 
+  		props: ['item','visible'],   		
+      computed: {				
+			},
+	    components: { 
+	    }
+  }
+</script>
+<style scoped>
+	
+</style>
+
+
